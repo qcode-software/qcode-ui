d25354d9eb4063b403286d8c0e835b1094347f4d
daniel <daniel@c248db91-1130-4c22-bb80-59636df81b6c>
2012-05-29 13:15:58 +0000

    cleanup JavaScript repository

---
+++ js-concat.tcl
@@ -0,0 +1,61 @@
+#!/usr/bin/tclsh8.5
+
+#| Concat all .js files in qcode directory
+
+set dir [file normalize [file dirname [info script]]]
+
+# Determinde the most recent version of qcode.js
+set previous_version 1.0
+foreach filename [glob $dir/qcode-?*.?*.js] {
+    regexp {qcode-([0-9]+\.[0-9]+).js} $filename -> version
+    if { $version > $previous_version } {
+	set previous_version $version
+    }
+}
+
+set major_version [lindex [split $previous_version .] 0]
+set minor_version [expr {[lindex [split $previous_version .] 1] + 1}]
+set version [join [list $major_version $minor_version] .]
+set out_filename qcode-$version.js
+set temp /tmp/$out_filename
+set out [open $temp w]
+
+# qcode.js description and liscense info
+puts $out {/*
+ * This file contains the concatented JavaScript Libary for Qcode Software Limited.
+ *
+ * https://svn.qcode.co.uk/js/trunk
+ * https://trac.qcode.co.uk/js
+ *
+ * Copyright (c) 2004-2012, Qcode Software Limited <hackers@qcode.co.uk> 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   - Neither the name of Qcode Software Limited nor the names of its contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+}
+
+foreach filename [lsort [glob $dir/qcode/*.js]] {
+    puts $out "/* ==== [file tail $filename] ==== */"
+    set fh [open $filename r]
+    puts $out [read $fh]
+    puts $out ""
+    close $fh
+}
+close $out
+
+exec cp $temp $dir/$out_filename
+
+file delete $temp
