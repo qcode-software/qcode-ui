3374bce6f234baf356dfb17480ac9ab1cea8fb4c
peter <peter@c248db91-1130-4c22-bb80-59636df81b6c>
2012-11-01 14:35:52 +0000

    Added disable/enable links plugin

---
+++ qcode/jquery.utils.js
@@ -0,0 +1,25 @@
+(function ($) {
+    $.fn.disable = function () {
+	return $(this).each(function () { 
+	    switch($(this)[0].nodeName.toUpperCase()) {
+	    case "A":
+		jQuery.data($(this)[0],"href",$(this).attr("href"));
+		$(this).removeAttr('href');
+	    default:
+		$(this).attr('disabled', 'disabled').addClass('disabled');
+	    }
+	});
+    };
+    $.fn.enable = function () {
+	return $(this).each(function () { 
+	    switch($(this)[0].nodeName.toUpperCase()) {
+	    case "A":
+		if ( typeof jQuery.data($(this)[0],"href")!="undefined" ) {
+		    $(this).attr("href",jQuery.data($(this)[0],"href"));
+		}
+	    default:
+		$(this).removeAttr('disabled').removeClass("disabled"); 
+	    }
+	});
+    };
+})(jQuery);
