a43051ce517cb622a9241cbf3a54c283335f1efe
daniel <daniel@c248db91-1130-4c22-bb80-59636df81b6c>
2012-12-05 16:18:32 +0000

    qcode-1.3.js - quirks mode navigate plugin

--- js-concat.tcl
+++ js-concat.tcl
@@ -8,7 +8,7 @@ set dir [file normalize [file dirname [info script]]]
 set last_major_version 1
 set last_minor_version 0
 foreach filename [glob $dir/qcode-?*.?*.js] {
-    regexp {qcode-([0-9]+)\.([0-9]+).js} $filename -> major_version minor_version
+    regexp {qcode-([0-9]+)\.([0-9]+).js$} $filename -> major_version minor_version
     if { $major_version >= $last_major_version && $minor_version > $last_minor_version } {
 	set last_minor_version $minor_version
 	set last_major_version $major_version
