2443bb6cada5fe4e91a47457afa77ca1a1e8c1a3
peter <peter@c248db91-1130-4c22-bb80-59636df81b6c>
2012-12-05 12:51:10 +0000

    Removed unused border-collapse script

--- qcode-2.2.js
+++ qcode-2.2.js
@@ -5022,50 +5022,6 @@ function dynamicResize(oContainer) {
 }
 
 
-/* ==== jquery.borderCollapse.js ==== */
-(function($){
-    $.fn.borderCollapse = function(){
-	var tables = $(this).filter('table').filter(function(){return $(this).css('border-collapse') == "collapse";});
-	tables.each(function(i, element) {
-	    var table = $(element);
-	    table.css({
-		'border-collapse': "separate",
-		'border-spacing': 0
-	    });
-	    var rows = $(table).find('tr:visible');
-	    if ( parseInt(table.css('border-top-width')) > 0 ) {
-		rows.eq(0).find('th, td').filter(':visible').css('border-top-width', 0);
-	    }
-	    if ( parseInt(table.css('border-left-width')) > 0 ) {
-		rows.each(function(j, element) {
-		    var row = $(element);
-		    row.find('th, td').filter(':visible').eq(0).css('border-left-width', 0);
-		});
-	    }
-	    if ( parseInt(table.css('border-right-width')) > 0 ) {
-		rows.each(function(j, element) {
-		    var row = $(element);
-		    row.find('th, td').filter(':visible').eq(-1).css('border-right-width', 0);
-		});
-	    }
-	    if ( parseInt(table.css('border-bottom-width')) > 0 ) {
-		rows.eq(-1).find('th, td').filter(':visible').css('border-bottom-width', 0);
-	    }
-	    rows.each(function(i, element){
-		var row = $(element);
-		var cells = row.find('td, th').filter(':visible');
-		cells.not(cells.eq(0)).css('border-left-width', 0);
-	    });
-	    rows.not(rows.eq(0)).find('th, td').css('border-top-width', 0);
-	});
-	
-    }
-})(jQuery);
-
-$(function(){
-    $('table:not(:has(tfoot))').css('border-bottom-width', 0);
-});
-
 /* ==== jquery.colInherit.js ==== */
 (function($) {
     $.fn.colInherit = function(options) {
