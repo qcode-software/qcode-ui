024726be588eefd80b164f1b0a027998dc8d2f59
peter <peter@c248db91-1130-4c22-bb80-59636df81b6c>
2012-12-19 11:49:49 +0000

    forgot to concat

--- qcode-2.7.js
+++ qcode-2.7.js
@@ -7549,12 +7549,63 @@ function dbFormHTMLArea(oDiv) {
 })(jQuery, window);
 
 
+/* ==== jquery.delayedGroupHover.js ==== */
+;(function($, window, undefined) {
+    // Delayed Group Hover plugin.
+    // Treats all the elements in the current jQuery object as a single "group";
+    // Invokes a user-defined callback when the user hovers over elements of the current jQuery object for more than a given time,
+    // or when they hover out of all the elements of the current jQuery object for more than a given time.
+    $.fn.delayedGroupHover = function(options) {
+	// hoverIn and hoverOut are optional callback functions.
+	var options = $.extend({
+	    inTime: 200,
+	    outTime: 200,
+	    hoverIn: undefined,
+	    hoverOut: undefined
+	}, options);
+
+	var timer;
+	var group = this;
+	function mouseEnter(event) {
+	    if ( $(event.relatedTarget).is(group) ) {
+		// mouse was already in the group
+		return;
+	    }
+	    if ( timer !== undefined ) {
+		// reset the timer
+		window.clearTimeout(timer);
+	    }
+	    if ( typeof options.hoverIn === "function" ) {
+		// schedule the hoverIn function to be called
+		timer = window.setTimeout(options.hoverIn.bind(group), options.inTime);
+	    }
+	}
+	function mouseLeave(event) {
+	    if ( $(event.relatedTarget).is(group) ) {
+		// mouse isn't leaving the group
+		return;
+	    }
+	    if ( timer !== undefined ) {
+		// reset the timer
+		window.clearTimeout(timer);
+	    }
+	    if ( typeof options.hoverOut === "function" ) {
+		// schedule the hoverOut function to be called
+		timer = window.setTimeout(options.hoverOut.bind(group), options.outTime);
+	    }
+	}
+	// bind events
+	group
+	    .on('mouseenter', mouseEnter)
+	    .on('mouseleave', mouseLeave);
+    }
+})(jQuery, window);
+
 /* ==== jquery.delayedHover.js ==== */
-(function($, window, undefined) {
+;(function($, window, undefined) {
     // Delayed hover plugin.
-    // Triggers "delayedHoverIn" or "delayedHoverOut" if the user hovers over, or out of, a single element for more than a given time.
-    // Each element in the current jQuery object is handled separately, but a selector can be used to delegate the listeners
-    // (eg. if delayedHover events will need to be triggered on elements that are added to the DOM later)
+    // Triggers custom events "delayedHoverIn" and "delayedHoverOut".
+    // Optionally takes a selector which tracks delayed mouse events on each element.
     $.fn.delayedHover = function(options){
 	var options = $.extend({
 	    inTime: 200,
@@ -7566,8 +7617,10 @@ function dbFormHTMLArea(oDiv) {
 	    var target = $(this);
 	    var timer = target.data('delayedHoverTimer');
 	    if ( timer !== undefined ) {
+		// reset the timer
 		window.clearTimeout(timer);
 	    }
+	    // schedule event to be triggered
 	    timer = window.setTimeout(function() {
 		target.trigger('delayedHoverIn');
 	    }, options.inTime)
@@ -7590,53 +7643,11 @@ function dbFormHTMLArea(oDiv) {
 		.on('mouseenter', mouseEnter)
 		.on('mouseleave', mouseLeave);
 	} else {
+	    // Apply to each element matching the selector
 	    $(this)
 		.on('mouseenter', options.selector, mouseEnter)
 		.on('mouseleave', options.selector, mouseLeave);
 	}
-    }
-
-    // Delayed Group Hover plugin.
-    // Treats all the elements in the current jQuery object as a single "group";
-    // Invokes a user-defined callback when the user hovers over one element from the current jQuery object for more than a given time,
-    // or when they hover out of all the elements of the current jQuery object for more than a given time.
-    $.fn.delayedGroupHover = function(options) {
-	// hoverIn and hoverOut are optional callback functions.
-	var options = $.extend({
-	    inTime: 200,
-	    outTime: 200,
-	    hoverIn: undefined,
-	    hoverOut: undefined
-	}, options);
-
-	var timer;
-	var group = this;
-	function mouseEnter(event) {
-	    if ( $(event.relatedTarget).is(group) ) {
-		return;
-	    }
-	    if ( timer !== undefined ) {
-		window.clearTimeout(timer);
-	    }
-	    if ( typeof options.hoverIn === "function" ) {
-		timer = window.setTimeout(options.hoverIn.bind(group), options.inTime);
-	    }
-	}
-	function mouseLeave(event) {
-	    if ( $(event.relatedTarget).is(group) ) {
-		return;
-	    }
-	    if ( timer !== undefined ) {
-		window.clearTimeout(timer);
-	    }
-	    if ( typeof options.hoverOut === "function" ) {
-		timer = window.setTimeout(options.hoverOut.bind(group), options.outTime);
-	    }
-	}
-
-	group
-	    .on('mouseenter', mouseEnter)
-	    .on('mouseleave', mouseLeave);
     }
 })(jQuery, window);
 
