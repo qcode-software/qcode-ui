8e62c9472a972d29038471a965c2de34c2fbbdc7
Peter Chaplin <peter@qcode.co.uk>
2013-02-07 12:18:51 +0000

    Make column_show_hide work in stadards mode

--- qcode-ui-2.10.js
+++ qcode-ui-2.10.js
@@ -1457,26 +1457,39 @@ function dynamicResize(oContainer) {
 /* ==== jquery.column_show_hide.js ==== */
 jQuery.fn.columns_show_hide = function(column_selector) {
     jQuery(this).each(function() {
-	var table = jQuery(this);				   
-	var hide_cols = jQuery(column_selector, table).filter(":visible");
-	var show_cols = jQuery(column_selector, table).filter(":hidden");
-	
+	var table = jQuery(this);
+        var toShow = $([]);
+        var toHide = $([]);
+
+	jQuery(column_selector, table).each(function() {
+            var column = jQuery(this);
+            var index = column.index();
+
+            if ( table.find('tbody>tr:first-child>td').eq(index).is(':visible') ) {
+                toHide = toHide.add(table.find('thead>tr>th:nth-child(' + (index + 1) + '), tbody>tr>td:nth-child(' + (index + 1) + '), tfoot>tr>td:nth-child(' + (index + 1) + ')'));
+
+            } else {
+                toShow = toShow.add(table.find('thead>tr>th:nth-child(' + (index + 1) + '), tbody>tr>td:nth-child(' + (index + 1) + '), tfoot>tr>td:nth-child(' + (index + 1) + ')'));
+            }
+        });
+
+
 	// Dettach table from DOM. 
 	var table_parent = table.parent();
 	var table_next_sibling = table.next();
 	table.detach();
-	
-	// show/hide columns ( >10 x faster operatoing on a detached DOM elements) 
-	hide_cols.hide(); 
-	show_cols.show(); 
-	
-	
+
+	toHide.css('display', "none");
+        toShow.css('display', "table-cell");
+
 	// Reattach table to it's original position in the DOM.
 	if (table_next_sibling.length) {
 	    table.insertBefore(table_next_sibling);
 	} else {		    
 	    table.appendTo(table_parent);
 	}
+
+        table.trigger('resize');
     });
 };
 
@@ -3915,6 +3928,7 @@ function dbFormHTMLArea(oDiv) {
             // Update the image when a new filename is chosen
             this.image = this.element.find('img');
             this._on(this.input, {
+                'comboSelect': this.loadImage,
                 'comboBlur': this.loadImage
             });
 
