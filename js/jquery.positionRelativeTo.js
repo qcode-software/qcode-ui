56527804cfe5b8b8c11cc0ab163693750420c2e7
Peter Chaplin <peter@qcode.co.uk>
2013-05-20 14:36:31 +0100

    Error handling for positionRelativeTo

--- js/jquery.positionRelativeTo.js
+++ js/jquery.positionRelativeTo.js
@@ -2,7 +2,13 @@
 // nb. if either element is in the offset parent chain of the other, position will account for scrolling of that element.
 (function ($, undefined) {
     $.fn.positionRelativeTo = function(target) {
+        if ( ! this.length ) {
+            $.error('positionRelativeTo called on empty object');
+        }
 	var target = $(target);
+        if ( ! target.length ) {
+            $.error('positionRelativeTo called with empty target');
+        }
 	var $body = $('body');
 
 	// Find chain of offset parents from this element to body
@@ -11,12 +17,18 @@
 	while ( ! current.is($body) ) {
 	    current = current.offsetParent();
 	    myOffsetParents = myOffsetParents.add(current);
+            if ( current.length !== 1 ) {
+                $.error('Offset chain error - perhaps positionRelativeTo was called on a detached object?');
+            }
 	}
 
 	// Search offset parents from target element up until a common offset parent is found
 	current = target;
 	while ( ! current.is(myOffsetParents) ) {
 	    current = current.offsetParent();
+            if ( current.length !== 1 ) {
+                $.error('Offset chain error - perhaps positionRelativeTo was called with a detached target?');
+            }
 	}
 	var commonOffsetParent = current;
 
@@ -30,7 +42,7 @@
 	    var positionOfCurrent = current.position();
 	    myPosition.left += positionOfCurrent.left;
 	    myPosition.top += positionOfCurrent.top;
-	    current = current.offsetParent();   
+	    current = current.offsetParent();
 	}
 	if ( ! (this.is(commonOffsetParent) || commonOffsetParent.is('body')) ) {
 	    myPosition.left += commonOffsetParent.scrollLeft();
@@ -47,7 +59,7 @@
 	    var positionOfCurrent = current.position();
 	    targetPosition.left += positionOfCurrent.left;
 	    targetPosition.top += positionOfCurrent.top;
-	    current = current.offsetParent();   
+	    current = current.offsetParent();
 	}
 	if ( ! (target.is(commonOffsetParent) || commonOffsetParent.is('body')) ) {
 	    targetPosition.left += commonOffsetParent.scrollLeft();
