3cbaba7740fcaff544081f2c8ce8a9d89ee9c234
Peter Chaplin <peter@qcode.co.uk>
2013-07-25 12:21:53 +0100

    Bug fix - flex db-grids have no status bar

--- qcode-ui-1.9.js
+++ qcode-ui-1.9.js
@@ -2487,7 +2487,7 @@ function dbGrid(oTable) {
       throw "This behavior requires a COLGROUP"
 	}
     // Parents Next Sibling should be the status Div containing a table
-      if ( jQuery(oTable).hasClass('db-grid') ) {
+      if ( ! jQuery(oTable).hasClass('flex') ) {
       oStatusTable = oTable.parentElement.nextSibling.firstChild;
     } else {
       oStatusTable = undefined;
