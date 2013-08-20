ba7fdb66d836bc8271f400dbd2918ac1c6978ff2
Peter Chaplin <peter@qcode.co.uk>
2013-08-20 15:21:46 +0100

    Concat

--- qcode-ui-3.0.js
+++ qcode-ui-3.0.js
@@ -6650,6 +6650,62 @@ function parseBoolean(value) {
     }
 })(jQuery);
 
+// setZeroTimeout / clearZeroTimeout
+// equivalent to setTimeout(function, 0) but uses window.postMessage to bypass browser minimum timeouts
+// In other words, schedule a function to be executed after all the other event handlers are finished
+// Does not take additional arguments (to pass additional arguments to the callback, use closures instead)
+(function(window, undefined) {
+    if ( window.postMessage ) {
+        var timeouts = []; // Array of functions
+        var ids = {}; // Hash of keys, used by clearZeroTimeout, referencing indices of timeouts
+        var messageName = "zero-timeout-message";
+        var nextID = 0;
+
+        function setZeroTimeout(fn) {
+            nextID++;
+            timeouts.push(fn);
+            ids[nextID] = timeouts.length - 1;
+            window.postMessage(messageName, "*");
+            return nextID;
+        }
+
+        function clearZeroTimeout(index) {
+            if ( ids[index] !== undefined ) {
+                timeouts.splice(ids[index], 1);
+                delete ids[nextID];
+            }
+        }
+
+        function handleMessage(event) {
+            if (event.source == window && event.data == messageName) {
+                event.stopPropagation();
+                if (timeouts.length > 0) {
+                    var fn = timeouts.shift();
+                    for (index in ids) {
+                        if ( ids[index] === timeouts.length ) {
+                            delete ids[index];
+                            break;
+                        }
+                    }
+                    fn();
+                }
+            }
+        }
+
+        window.addEventListener("message", handleMessage, true);
+
+        window.setZeroTimeout = setZeroTimeout;
+        window.clearZeroTimeout = clearZeroTimeout;
+    } else {
+        window.setZeroTimeout = function(fn) {
+            return window.setTimeout(fn, 0);
+        }
+        window.clearZeroTimeout = function(timeout) {
+            window.clearTimeout(timeout);
+        }
+    }
+})(window);
+
 /* ==== tableRowHighlight.js ==== */
 function tableRowHighlight(oTable) {
     jQuery(oTable).find("tr").click(function(event) {
