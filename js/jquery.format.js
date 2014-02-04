20ed543b5a23b8e44ee43591403511e943a9ff42
Peter Chaplin <peter@qcode.co.uk>
2014-02-04 17:12:45 +0000

    EoD for format plugin

---
+++ js/jquery.format.js
@@ -0,0 +1,109 @@
+;(function($, undefined) {
+    var lan = navigator.language || navigator.userLanguage;
+    $.fn.format = function(settings) {
+        var options = $.extend({
+            sigfigs: null,
+            dps: null,
+            zeros: "auto",
+            function: undefined
+        }, settings);
+        this.each(function(index, element) {
+            // Get the original text value of the element (and store in $.data)
+            var originalData = $(element).data('qcode-format-original-data');
+            if ( originalData === undefined ) {
+                originalData = $(element).text().trim();
+                $(element).data('qcode-format-original-data', originalData);
+            }
+
+            // Decide whether or not to show zeros
+            if ( options.zeros === "auto" ) {
+                if ( originalData === "" ) {
+                    var zeros = false;
+                } else {
+                    var zeros = true
+                }
+            } else {
+                zeros = options.zeros;
+            }
+
+            // Custom function
+            if ( options.function !== undefined ) {
+                $(element).text(options.function.call(element, originalData));
+
+            } else {
+                // Extract numeric value
+                var value = originalData.replace(/[^0-9.]/g, '') * 1;
+
+                // Leave non-numeric values alone
+                if ( Number.isNaN(value) ) {
+                    return;
+                }
+
+                // Hide zeros if option is selected
+                if ( value * 1 === 0 && ! zeros ) {
+                    $(element).text("");
+                    return;
+                }
+
+                // Round to significant figures (but display as number, not using scientific notation)
+                if ( options.sigfigs !== null ) {
+                    value = value.toPrecision(options.sigfigs) * 1;
+                }
+
+                // Round to decimal places, and pad with trailing 0s
+                if ( options.dps !== null ) {
+                    value = value.toFixed(options.dps);
+                }
+
+                $(element).text(value);
+            }
+        });
+        return this;
+    };
+})(jQuery);
+
+(function(undefined) {
+    try {
+        (0).toLocaleString("");
+    } catch (e) {
+        if ( e.name === "RangeError" ) {
+            Number.prototype.toLocaleString = function(locales, options) {
+                var minIntDigs = options.minumumIntegerDigits;
+                var minFraDigs = options.minumumFractionDigits;
+                var maxFraDigs = options.maximumFractionDigits;
+                var minSigFigs = options.minimumSignificantDigits;
+                var maxSigFigs = options.maximumSignificantDigits;
+
+                if ( minSigFigs !== undefined || maxSigFigs !== undefined ) {
+                    minSigFigs = coalesce(minSigFigs,1);
+                    maxSigFigs = coalesce(maxSigFigs,minSigFigs);
+                    if ( minSigFigs !== parseInt(minSigFigs)
+                       || minSigFigs > 21
+                       || minSigFigs < 0 ) {
+                        throw new RangeError('minimumSignificantDigits value is out of range');
+                    }
+                    if ( maxSigFigs !== parseInt(maxSigFigx)
+                         || maxSigFigs < minSigFigs
+                         || maxSigFigs > 21 ) {
+                        throw new RangeError('maximumSignificantDigits value is out of range');
+                    }
+                    var string = (this.toPrecision(maxSigFigs) * 1).toString();
+                    var parts = string.split('.');
+                    var remainder = parts[0];
+                    var groups = [];
+                    while ( remainder.length > 3 ) {
+                        groups.unshift(remainder.slice(-3));
+                        remainder = remainder.slice(0, -3);
+                    }
+                    if ( remainder.length > 0 ) {
+                        groups.unshift(remainder);
+                    }
+                    
+
+                } else {
+                    
+                }
+            }
+        }
+    }
+})();
