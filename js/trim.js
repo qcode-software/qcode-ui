7755d86db75af99c9e74ae6c484d14453a591b64
Peter Chaplin <peter@qcode.co.uk>
2014-02-05 12:12:33 +0000

    Comments

--- js/trim.js
+++ js/trim.js
@@ -1,3 +1,7 @@
+/* ===================================================================
+   Backwards-compatible support for String.prototype.trim()
+   Remove leading and trailing whitespace from a string
+=================================================================== */
 if ( ! String.prototype.trim ) {
     String.prototype.trim = function() {
         return this.replace(/^\s+|\s+$/gm, '');
