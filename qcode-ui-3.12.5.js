499018771a55a1ee7b70c4c295fd572a0b39972a
Peter Chaplin <peter@qcode.co.uk>
2014-07-30 15:05:19 +0100

    Concat

--- qcode-ui-3.12.5.js
+++ qcode-ui-3.12.5.js
@@ -24,75 +24,7 @@
 */
 
 /* ==== 0.jquery-hacks.js ==== */
-// Bug fix for table border width detection in ie9
 (function($){
-    if ( $.browser.msie && parseInt($.browser.version, 10) == "9" ) {
-        var oldCssFunction = $.fn.css;
-        $.fn.css = function() {
-            if ( this.first().is('table') && arguments.length == 1 ) {
-		var table = this.first();
-                switch(arguments[0]){
-                case "border-left-width":
-		case "borderLeftWidth":
-                    var totalBorderWidth = parseInt(this[0].offsetWidth) - getInnerWidth(table);
-                    this.css('border-left-width', 0);
-                    var newTotalBorderWidth = parseInt(this[0].offsetWidth) - getInnerWidth(table);
-                    var borderWidth = totalBorderWidth - newTotalBorderWidth;
-                    this.css('border-left-width', borderWidth);
-                    return borderWidth + "px";
-                    
-                case "border-right-width":
-		case "borderRightWidth":
-                    var totalBorderWidth = parseInt(this[0].offsetWidth) - getInnerWidth(table);
-                    this.css('border-right-width', 0);
-                    var newTotalBorderWidth = parseInt(this[0].offsetWidth) - getInnerWidth(table);
-                    var borderWidth = totalBorderWidth - newTotalBorderWidth;
-                    this.css('border-right-width', borderWidth);
-                    return borderWidth + "px";
-                    
-                case "border-top-width":
-		case "borderTopWidth":
-                    var totalBorderWidth = parseInt(this[0].offsetHeight) - getInnerHeight(this);
-                    this.css('border-top-width', 0);
-                    var newTotalBorderWidth = parseInt(this[0].offsetHeight) - getInnerHeight(this);
-                    var borderWidth = totalBorderWidth - newTotalBorderWidth;
-                    this.css('border-top-width', borderWidth);
-                    return borderWidth + "px";
-                    
-                case "border-bottom-width":
-		case "borderBottomWidth":
-                    var totalBorderWidth = parseInt(this[0].offsetHeight) - getInnerHeight(this);
-                    this.css('border-bottom-width', 0);
-                    var newTotalBorderWidth = parseInt(this[0].offsetHeight) - getInnerHeight(this);
-                    var borderWidth = totalBorderWidth - newTotalBorderWidth;
-                    this.css('border-bottom-width', borderWidth);
-                    return borderWidth + "px";
-                    
-                default:
-                    return oldCssFunction.apply(this,arguments);
-                }
-            } else {
-                return oldCssFunction.apply(this,arguments);
-            }
-        };
-    }
-    function getInnerWidth(table) {
-        var borderSpacing = table.css('border-spacing');
-        var horizontalSpacing = borderSpacing.split(' ').shift();
-        return parseInt(table.find('tbody').outerWidth()) + (parseInt(horizontalSpacing) * 2);
-    }
-    function getInnerHeight(table) {
-        var borderSpacing = table.css('border-spacing');
-        var verticalSpacing = parseInt(borderSpacing.split(' ').pop());
-        var totalHeight = verticalSpacing;
-        table.find('thead, tbody, tfoot').each(function(){
-            if ( $(this).css('position') != "absolute" ) {
-                totalHeight += parseInt($(this).outerHeight()) + verticalSpacing;
-            }
-        });
-        return totalHeight;
-    }
-
     jQuery.expr[":"].focus = function( elem ) {
 	var doc = elem.ownerDocument;
 	return elem === doc.activeElement && (!doc.hasFocus || doc.hasFocus()) && !!(elem.type || elem.href || ~elem.tabIndex || elem.isContentEditable);
@@ -1752,7 +1684,7 @@ function dynamicResize(oContainer) {
 	}, 
 	repaint: function() {
 	    // repaint the editor
-	    if ( this.currentElement.length == 1 ) {
+	    if ( this.currentElement.length == 1 && this.editor.css('display') !== 'none' ) {
 		var editor = this.editor;
 		var element = this.currentElement;
 
@@ -2031,7 +1963,7 @@ function dynamicResize(oContainer) {
 	},
 	repaint: function() {
 	    // repaint the editor
-	    if ( this.currentElement.length == 1 ) {
+	    if ( this.currentElement.length == 1 && this.editor.css('display') !== 'none' ) {
 		var editor = this.editor;
 		var comboOptions = this.comboOptions;
 		var element = this.currentElement;
@@ -2384,7 +2316,7 @@ function dynamicResize(oContainer) {
 	    this.editor.hide();
 	},
 	repaint: function() {
-	    if ( this.currentElement.length == 1 ) {
+	    if ( this.currentElement.length == 1 && this.editor.css('display') !== 'none' ) {
 		// Copy various style from the target element to the editor
 		var editor = this.editor;
 		var element = this.currentElement;
@@ -2630,7 +2562,7 @@ function dynamicResize(oContainer) {
 	},
 	repaint: function() {
 	    // repaint the editor
-	    if ( this.currentElement.length == 1 ) {
+	    if ( this.currentElement.length == 1 && this.editor.css('display') !== 'none' ) {
 		var editor = this.editor;
 		var element = this.currentElement;
 
@@ -2838,7 +2770,7 @@ function dynamicResize(oContainer) {
 	    this.editor.hide();
 	},
 	repaint: function() {
-	    if ( this.currentElement.length == 1 ) {
+	    if ( this.currentElement.length == 1 && this.editor.css('display') !== 'none' ) {
 		var editor = this.editor;
 		var element = this.currentElement;
 		// Copy various style from the target element to the editor
@@ -5960,13 +5892,17 @@ uses the existing id if it has one
         if ( ! target.length ) {
             $.error('positionRelativeTo called with empty target');
         }
-	var $body = $('body');
 
+        var root = $('<div>').offsetParent();
+        
 	// Find chain of offset parents from this element to body
 	var myOffsetParents = this;
 	var current = this;
-	while ( ! current.is($body) ) {
+	while ( ! current.is(root) ) {
 	    current = current.offsetParent();
+            if ( current.is(myOffsetParents) ) {
+                $.error('positionRelativeTo Looping error');
+            }
 	    myOffsetParents = myOffsetParents.add(current);
             if ( current.length !== 1 ) {
                 $.error('Offset chain error - perhaps positionRelativeTo was called on a detached object?');
@@ -5975,7 +5911,7 @@ uses the existing id if it has one
 
 	// Search offset parents from target element up until a common offset parent is found
 	current = target;
-	while ( ! current.is(myOffsetParents) ) {
+	while (( ! current.is(myOffsetParents)) && ( ! current.is(root)) ) {
 	    current = current.offsetParent();
             if ( current.length !== 1 ) {
                 $.error('Offset chain error - perhaps positionRelativeTo was called with a detached target?');
@@ -6001,7 +5937,7 @@ uses the existing id if it has one
             }
 	    current = current.offsetParent();
 	}
-	if ( ! (this.is(commonOffsetParent) || commonOffsetParent.is('body')) ) {
+	if ( ! (this.is(commonOffsetParent) || commonOffsetParent.is(root)) ) {
 	    myPosition.left += commonOffsetParent.scrollLeft();
 	    myPosition.top += commonOffsetParent.scrollTop();
 	}
@@ -6024,7 +5960,7 @@ uses the existing id if it has one
             }
 	    current = current.offsetParent();
 	}
-	if ( ! (target.is(commonOffsetParent) || commonOffsetParent.is('body')) ) {
+	if ( ! (target.is(commonOffsetParent) || commonOffsetParent.is(root)) ) {
 	    targetPosition.left += commonOffsetParent.scrollLeft();
 	    targetPosition.top += commonOffsetParent.scrollTop();
 	}
