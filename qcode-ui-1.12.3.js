8f00148c0e508ede26f6a0d397e35bdc330a4cac
Daniel Clark <daniel@qcode.co.uk>
2014-08-07 15:06:31 +0100

    release 1.12.3

--- qcode-ui-1.12.3.js
+++ qcode-ui-1.12.3.js
@@ -224,7 +224,7 @@ function focusFirstChild(elmt) {
 }
 
 function urlSet(url,name,value) {
-	var re = /([^\?]+)\??(.*)/;
+	var re = /([^\?]+)\??([^\#]*)/;
 	re.exec(url);
 	var path = RegExp.$1;
 	var queryString = RegExp.$2;
@@ -257,7 +257,7 @@ function urlDataSet(data,name,value) {
 }
 
 function urlGet(url,name) {
-  var re = /([^\?]+)\??(.*)/;
+  var re = /([^\?]+)\??([^\#]*)/;
   re.exec(url);
   var path = RegExp.$1;
   var queryString = RegExp.$2;
