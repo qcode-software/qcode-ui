95bf9d65b2cc047528ff5df2996e55cd9e5201aa
Daniel Clark <daniel@qcode.co.uk>
2014-08-08 09:26:43 +0100

    release 3.13.3

--- qcode-ui-3.13.3.js
+++ qcode-ui-3.13.3.js
@@ -8233,7 +8233,7 @@ function unescapeHTML(str) {
 };
 
 function urlSet(url,name,value) {
-    var re = /([^\?]+)(?:\?([^\#]*))?(\#.*)?/;
+    var re = /([^\?\#]+)(?:\?([^\#]*))?(\#.*)?/;
     re.exec(url);
     var path = RegExp.$1;
     var queryString = RegExp.$2;
