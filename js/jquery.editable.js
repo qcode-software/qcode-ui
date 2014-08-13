50b26bb3eef7280e305246b7b6a34b3ee2fadd1e
Peter Chaplin <peter@qcode.co.uk>
2014-08-13 09:33:19 +0100

    Code review changes

--- js/jquery.editable.js
+++ js/jquery.editable.js
@@ -1,61 +1,73 @@
 // editable
 // - plugin to standardise editable elements
 // - uses dbEditor plugins to make non-form elements, such as <div>s, editable
+// Exposes methods for setValue, getValue, setRange (the selected text range), and hasFocus.
 
-// fire
-// valueChange
+// fires
+// editorValueChange event
 ;(function($, undefined) {
     $.widget('qcode.editable', {
         options: {
-            // type - type of editor
+            // editorType - type of editor (or "input" for no editor)
             // currently one of "auto","input","text","combo","bool","textarea","htmlarea"
-            type: "auto",
+            // "auto" defaults to "input" for form elements, and otherwise looks for a "type" attribute (defaulting to "text")
+            editorType: "auto",
 
             // defaultRange - range to select on focus. currently one of null,"start","end","all"
             // (null defaults to "all" for non-form elements because the browser won't put the
             // cursor to where the mouse was clicked.
             defaultRange: null,
+
+            // container for the editor plugins. Should be a the target element's offset parent or a descendent thereof.
+            // defaults to the element's offset parent.
             container: null
         },
         _create: function() {
-            if ( this.options.type === "auto" ) {
+            // Replace "auto" editorType with default
+            if ( this.options.editorType === "auto" ) {
                 if ( this.element.is(':input') ) {
-                    this.options.type = "input";
+                    this.options.editorType = "input";
                 } else {
                     switch ( this.element.attr('type') ) {
                     case "combo":
                     case "bool":
                     case "textarea":
                     case "htmlarea":
-                        this.options.type = this.element.attr('type');
+                        this.options.editorType = this.element.attr('type');
                         break;
                     case "text":
                     case undefined:
-                        this.options.type = "text";
+                        this.options.editorType = "text";
                         break;
                     default:
                         $.error('Unknown editor type');
                     }
                 }
             }
-            if ( this.options.type !== "input"
+
+            // defaultRange for non-form elements
+            if ( this.options.editorType !== "input"
                  && this.options.defaultRange === null
                ) {
                 this.options.defaultRange = "all";
             }
+
+            // default container
             if ( this.options.container === null ) {
                 this.options.container = this.element.offsetParent();
             }
             if ( $(this.options.container).is('html') ) {
                 this.options.container = $('body');
             }
+
+            // event listeners
             this._on({
                 'focus': this._onFocus,
                 'cut': this._onCut,
                 'paste': this._onPaste,
                 'keyup': this._onKeyUp
             });
-            if ( this.options.type !== "input" ) {
+            if ( this.options.editorType !== "input" ) {
                 this._on({
                     'editorBlur': function() {
                         this._editor('hide');
@@ -69,14 +81,15 @@
                     }
                 });
             }
+
+            // Initialised on already-focussed element (probably in response to focus event)
             if ( this.element.is(':focus') ) {
-                // Initialised on already-focussed element (probably in response to focus event)
                 this._onFocus();
             }
         },
         _onFocus: function() {
             if ( ! this.element.is('[disabled]') ) {
-                if ( this.options.type !== "input" ) {
+                if ( this.options.editorType !== "input" ) {
                     this._editor('show', this.element, this.getValue());
                 }
                 if ( this.options.defaultRange !== null ) {
@@ -105,18 +118,18 @@
         },
         hasFocus: function() {
             return ( this.element.is(':focus') 
-                     || ( this.options.type !== "input"
+                     || ( this.options.editorType !== "input"
                           && this._editor('getCurrentElement').is(this.element)
                         )
                    );
         },
         getValue: function() {
-            if ( this.options.type !== "input"
+            if ( this.options.editorType !== "input"
                  && this._editor('getCurrentElement').is(this.element)
                ) {
                 return this._editor('getValue');
 
-            } else if ( this.options.type === "htmlarea" ) {
+            } else if ( this.options.editorType === "htmlarea" ) {
                 return this.element.html();
 
             } else if ( this.element.is(':input') ) {
@@ -127,14 +140,14 @@
             }
         },
         setValue: function(newValue) {
-            if ( this.options.type !== "input"
+            if ( this.options.editorType !== "input"
                  && this._editor('getCurrentElement').is(this.element)
                ) {
                 this._editor('setValue',newValue);
                 return
             }
             
-            if ( this.options.type === "htmlarea" ) {
+            if ( this.options.editorType === "htmlarea" ) {
                 this.element.html(newValue);
 
             } else if ( this.element.is(':input') ) {
@@ -145,7 +158,7 @@
             }
         },
         setRange: function(newRange) {
-            if ( this.options.type !== "input" 
+            if ( this.options.editorType !== "input" 
                  && this._editor('getCurrentElement').is(this.element) ) {
                 this._editor('selectText', newRange);
 
@@ -166,7 +179,7 @@
             }
         },
         _editor: function(args) {
-            switch(this.options.type) {
+            switch(this.options.editorType) {
             case "combo":
                 pluginName="dbEditorCombo";
                 break;
