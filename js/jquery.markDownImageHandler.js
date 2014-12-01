4d7bc7a299b5fa5eba2dd14fe54aef9a846e896c
Peter Chaplin <peter@qcode.co.uk>
2014-12-01 14:31:58 +0000

    bug fix

--- js/jquery.markDownImageHandler.js
+++ js/jquery.markDownImageHandler.js
@@ -31,50 +31,50 @@ $.fn.markDownImageHandler = function(options) {
                         var ext = '';
                     }
                     file.name = guidGenerate() + ext;
-                    handleFiles([file], $textarea, options);
+                    handleFiles([file]);
                 }
             };
         });
+
+        function handleFiles(fileList) {
+            var index = $textarea.textrange('get').selectionStart;
+            $.each(fileList, function (i, file) {
+                var head = $textarea.val().slice(0, index);
+                var tail = $textarea.val().slice(index);
+
+                var tag = "![Uploading " + file.name + " 0%]()";
+                var tagPattern = new RegExp('!\\[Uploading ' + file.name + ' [0-9]+%\\]\\(\\)');
+                $textarea.val(head + tag + tail);
+
+                var uploader = new qcode.Uploader({
+                    file: file,
+                    chunkSize: options.chunkSize,
+                    url: options.uploadURL,
+                    postData: options.postData
+                });
+
+                $(uploader)
+                        .on('progress', function(event) {
+                            var perct = parseInt((event.loaded / event.total) * 100);
+                            $textarea.val(
+                                $textarea.val().replace(tagPattern, "![Uploading " + file.name + " " + perct + "%]()")
+                            );
+                        })
+                        .on('complete', function(event, xhr) {
+                            var url = options.getImageURL(xhr, file);
+                            $textarea.val(
+                                $textarea.val().replace(tagPattern, '![image](' + url + ')')
+                            );
+                        })
+                        .on('error', function(event, xhr) {
+                            $textarea.val(
+                                $textarea.val().replace(tagPattern, '![Error uploading ' + file.name + ']()')
+                            );
+                            $textarea.trigger('error', [xhr]);
+                        });
+                uploader.start();
+            });
+        };
     });
     return this;
-
-    function handleFiles(fileList, $textarea, options) {
-        $.each(fileList, function (i, file) {
-            var index = $textarea.textrange('get').selectionStart;
-            var head = $textarea.val().slice(0, index);
-            var tail = $textarea.val().slice(index);
-
-            var tag = "![Uploading " + file.name + " 0%]()";
-            var tagPattern = new RegExp('!\\[Uploading ' + file.name + ' [0-9]+%\\]\\(\\)');
-            $textarea.val(head + tag + tail);
-
-            var uploader = new qcode.Uploader({
-                file: file,
-                chunkSize: options.chunkSize,
-                url: options.uploadURL,
-                postData: options.postData
-            });
-
-            $(uploader)
-                    .on('progress', function(event) {
-                        var perct = parseInt((event.loaded / event.total) * 100);
-                        $textarea.val(
-                            $textarea.val().replace(tagPattern, "![Uploading " + file.name + " " + perct + "%]()")
-                        );
-                    })
-                    .on('complete', function(event, xhr) {
-                        var url = options.getImageURL(xhr, file);
-                        $textarea.val(
-                            $textarea.val().replace(tagPattern, '![image](' + url + ')')
-                        );
-                    })
-                    .on('error', function(event, xhr) {
-                        $textarea.val(
-                            $textarea.val().replace(tagPattern, '![Error uploading ' + file.name + ']()')
-                        );
-                        $textarea.trigger('error', [xhr]);
-                    });
-            uploader.start();
-        });
-    };
 };
