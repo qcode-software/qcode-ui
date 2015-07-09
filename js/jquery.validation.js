01b355e23d195d83ec86cde1a6c6db18fcfdf8e0
Nicky Johnstone <nicky@qcode.co.uk>
2015-07-09 15:50:40 +0100

    Interim commit

--- js/jquery.validation.js
+++ js/jquery.validation.js
@@ -15,11 +15,21 @@
                     ready: true
                 },
                 hide: {
-                    event: 'focus blurs reset keydown paste cut',
+                    event: 'click focus blurs reset keydown paste cut',
                     delay: 0
                 },
                 style: {
                     classes: 'qtip-qcode'
+                },
+                events: {
+		    render: function(event, api) {
+                        // Clicking on the tooltip causes the target element to gain focus and hides the tooltip.
+			api.elements.tooltip.on('click', function(event) {
+                            api.elements.target.focus();
+                            // Call the hide method in case the default hide events were overwritten
+                            api.hide();
+                        });
+		    }
                 }
             },
             hints: {},
@@ -37,6 +47,7 @@
         },
         
         _create: function() {
+            this.message = [];
             // Click handlers for submit buttons on the form.
             // Used to add hidden input elements with the button's name and value because jQuery form.serialize() function does not
             // include submit button data since it has no way of knowing which button was used to submit the form.
@@ -55,10 +66,12 @@
                 // Stop the form submission.
                 event.preventDefault();
                 var $form = $(this);
+                
                 // Get rid of old messages
                 $form.validation('hideMessage', 'alert');
                 $form.validation('hideMessage', 'notify');
                 $form.validation('hideMessage', 'error');
+                
                 // POST the form data
                 var path = $form.attr('action');
                 $.ajax({
@@ -70,25 +83,33 @@
                         $form.validation('parseResponse', response);
                     },
                     error: function(jqXHR, textStatus, errorThrown) {
-                        // HTTP ERROR
-	                if ( jqXHR.status != 200 && jqXHR.status != 0 ) {
-                            $form.validation('parseResponse', $.parseJSON(jqXHR.responseText));
-                            return;
-                        }
-
-                        switch (textStatus) {
-                        case "parseerror":
-                            $form.validation('showMessage', 'error', "Couldn't parse the response.");
-                            break;
-                        case "timeout":
-                            $form.validation('showMessage', 'error', "Request timed out.");
-                            break;
-                        case "abort":
-                            $form.validation('showMessage', 'error', "Request was aborted.");
+                        var returnType = jqXHR.getResponseHeader('content-type');
+                        switch (returnType) {
+                        case "application/json; charset=utf-8":
+                            // HTTP ERROR
+	                    if ( jqXHR.status != 200 && jqXHR.status != 0 ) {
+                                $form.validation('parseResponse', $.parseJSON(jqXHR.responseText));
+                            } else {
+                                switch (textStatus) {
+                                case "parseerror":
+                                    $form.validation('showMessage', 'error', "Couldn't parse the response.");
+                                    break;
+                                case "timeout":
+                                    $form.validation('showMessage', 'error', "Request timed out.");
+                                    break;
+                                case "abort":
+                                    $form.validation('showMessage', 'error', "Request was aborted.");
+                                    break;
+                                default:
+                                    $form.validation('showMessage', 'error', "An error occurred: " + errorThrown);
+                                }
+                            }
                             break;
                         default:
-                            $form.validation('showMessage', 'error', "An error occurred: " + errorThrown);
+                            $form.validation('showMessage', 'error', 'Expected JSON but got ' + returnType);
                         }
+
+                        scrollToElement($form.validation('getMessage', 'error'), 200);
                     }
                 });
             }
@@ -99,39 +120,96 @@
         parseResponse: function(response) {
             // Parses the response to show qtips and messages where necessary.
             var $form = $(this.element);
-            var allValid = true;
+            var $scrollElement = undefined;
             // Check each record item is valid.
             $.each(response.record, function (name, object) {
                 var $element = $form.find('[name=' + name + ']');
                 if ( ! object.valid ) {
                     // Record item not valid - mark invalid and display message to user.
-                    allValid = false;
                     if ( $element.length !== 0 ) {
                         $form.validation('showValidationMessage', $element, object.message);
                         $element.addClass('invalid');
+
+                        // Compare to highest element on page so far
+                        if ( typeof $scrollElement === 'undefined' || $element.offset().top < $scrollElement.offset().top ) {
+                            $scrollElement = $element;
+                        }
                     }
                 } else {
                     $element.removeClass('invalid');
                 }
             });
+
+            // Show messages if action redirect is not given
+            if (response.message && (!response.action || !response.action.redirect) ) {
+                $.each(response.message, function(type, object) {
+                    $form.validation('showMessage', type, object.value);
+                    // Compare message to highest element on page
+                    var $message = $form.validation('getMessage', type);
+                    if ( typeof $scrollElement === 'undefined' ||
+                         ($message.is(':visible') && $message.offset().top < $scrollElement.offset().top) ) {
+                        $scrollElement = $message;
+                    }
+                });
+            }
             
-            if ( allValid && response.status === 'valid') {
-                if (response.action && response.action['redirect']) {
+            if ( response.status === 'valid') {
+                if (response.action && response.action.redirect) {
                     // Redirect if record was valid and the redirect action was given
-                    window.location.href = response.action['redirect'].value;
-                } else if (response.message['notify']) {
-                    $form.validation('showMessage', 'notify', response.message['notify'].value);
-                    $form.validation('reposition');
+                    window.location.href = response.action.redirect.value;
                 }
                 // Reset the form
                 $form.trigger('reset');
                 
-            } else if (response.message) {
-                $.each(response.message, function(type, object) {
-                    $form.validation('showMessage', type, object.value);
-                    $form.validation('reposition');
-                });
             }
+
+            // Scroll to the element if there is one to scroll to
+            if ( typeof $scrollElement !== 'undefined' && typeof $scrollElement.qtip('api') !== 'undefined' ) {
+                // $scrollElement exists and has has a qtip
+                
+                var scrollToHighest = function(api) {
+                    // Scrolls to the highest element on the page out of three possible elements:
+                    // the qtip, the element the qtip is bound to, and the label for the element if it has one.
+                    
+                    // Check if element has a label
+                    var $label = $form.find('label[for=' + $scrollElement.attr('id') + ']');
+                    if ( $label.length === 0 ) {
+                        $label = $scrollElement.closest('label');
+                    }
+
+                    // If a label was found then check if the label is higher than the element on the page
+                    if ( $label.length > 0 && $label.offset().top < $scrollElement.offset().top ) {
+                        $scrollElement = $label
+                    }
+                    
+                    // Compare the top offset of highest elements to the qtip tooltip
+                    if ( api.tooltip.offset().top < $scrollElement.offset().top ) {
+                        $scrollElement = api.tooltip;
+                    }
+
+                    scrollToElement($scrollElement, 200);
+                }
+
+                if ( $scrollElement.qtip('api').rendered ) {
+                    // Qtip for element has been rendered so can scroll to it
+                    scrollToHighest($scrollElement.qtip('api'));
+                } else {
+                    // Qtip hasn't been rendered yet - listen for render event then scroll to element
+                    $scrollElement.qtip('api').set('events.render', function(event, api) {
+                        // Clicking on the tooltip causes the target element to gain focus and hides the tooltip.
+			api.elements.tooltip.on('click', function(event) {
+                            api.elements.target.focus();
+                            // Call the hide method in case the default hide events were overwritten
+                            api.hide();
+                        });
+                        scrollToHighest(api);
+		    });
+                }
+            } else if ( typeof $scrollElement !== 'undefined' ) {
+                // $scrollElement exists but has no qtip - scroll to the $scrollElement
+                scrollToElement($scrollElement, 200);
+            }
+            
             // Trigger validation completion event
             $form.trigger({
                 type: 'validationComplete',
@@ -174,12 +252,12 @@
         
         showMessage: function(type, message) {
             // Show the message of the type given with message as the content.
-            if (! this[type]) {
+            if (! this.message[type]) {
                 // Message area doesn't exist so create it.
                 var messageDiv = $('<div></div>').addClass(this.options.messages[type].classes)
                 var messageContent = $('<span></span>').html(message).addClass('message-content');
                 
-                messageDiv.append(messageContent);
+                messageDiv.append(messageContent).hide();
                 var validationElement = $(this.element);
                 messageDiv.click(function(event){
                     validationElement.validation('hideMessage', type);
@@ -194,19 +272,31 @@
                     $('body').append(messageDiv);
                 }
 
-                this[type] = messageDiv;
+                this.message[type] = messageDiv;
+                messageDiv.show(200, $.proxy(function() {
+                    this.reposition();
+                }, this));
             } else {
                 // Update the message
-                $('.' + type + ' > .message-content').html(message);
+                this.message[type].find('.message-content').text(message);
+                this.message[type].show(200, $.proxy(function() {
+                    this.reposition();
+                }, this));
             }
         },
         
         hideMessage: function(type) {
             // Hide the message with the given type.
-            if (this[type]) {
-                this[type].remove();
-                this[type] = undefined;
+            if (this.message[type]) {
+                this.message[type].hide(100, $.proxy(function() {
+                    this.reposition();
+                }, this));
             }
+        },
+
+        getMessage: function(type) {
+            // Returns the jquery object for the message of the given type
+            return this.message[type];
         },
         
         reposition: function() {
@@ -234,9 +324,9 @@
         
         _messagesDestroy: function() {
             // Remove all messages added by this plugin.
-            this['alert'].remove();
-            this['error'].remove();
-            this['notify'].remove();
+            this.message['alert'].remove();
+            this.message['error'].remove();
+            this.message['notify'].remove();
         }
         
     });
