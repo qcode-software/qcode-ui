85f0d98ea785cc12f4661137ac51b5f0fa9fef70
Nicky Johnstone <nicky@qcode.co.uk>
2015-07-21 17:23:35 +0100

    EOD

--- js/jquery.actionConfirm.js
+++ js/jquery.actionConfirm.js
@@ -1,21 +1,21 @@
 // actionConfirm plugin
-// Call on <a>, generates a modal dialog asking the user to confirm when the user tries to use the link.
+// Call on <a> or <button>
+// Prompts user with a dialog box to confirm their action.
+// If the user confirms then the form is submitted if a button otherwise navigates to the anchor href.
 ;(function($, undefined) {
     $.fn.actionConfirm = function() {
-        
-        var action = function(event) {
+        this.on('click', function(event) {
             event.preventDefault();
 	    var $element = $(this);
 
             if ( ( ! $element.is('.disabled')) ) {
+                var title = $element.text();
+                var text = 'Are you sure you want to ' + coalesce($element.attr('title'), $element.text()) + '?';
                 var tag = $element.prop('tagName');
                 switch(tag) {
-                case "FORM":
-                    var title = 'Confirm',
-                    var text = 'Are you sure you want to carry out that action?';
+                case "BUTTON":
                     var yesFunction = function() {
-                        $element.off('submit.actionConfirm');
-                        $element.submit();
+                        $element.closest('form').submit();
                     }
                     
                     break;
@@ -23,13 +23,14 @@
                     if ( ! $element.attr('href') ) {
                         return;
                     }
-                    var title = $element.text();
-                    var text = 'Are you sure you want to ' + coalesce($element.attr('title'), $element.text()) + '?';
+                    
                     var yesFunction = function() {
                         window.location = $element.attr('href');
                     }
                     
                     break;
+                default:
+                    return;
                 }
                 
 	        $('<div>')
@@ -47,12 +48,6 @@
 		            height: 200
 		        });
 	    }
-        }
-        
-        if ( this.is('form') ) {
-            this.on('submit.actionConfirm', action);
-        } else if ( this.is('a') ) {
-            this.on('click', action);
-        }
+        });
     }
 })(jQuery);
