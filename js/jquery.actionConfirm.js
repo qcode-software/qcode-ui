6a477eb6d95af37025c45fe2297b26378cbd20f9
Nicky Johnstone <nicky@qcode.co.uk>
2015-11-20 15:40:24 +0000

    refactor common code

--- js/jquery.actionConfirm.js
+++ js/jquery.actionConfirm.js
@@ -9,84 +9,76 @@
         _create: function() {
             
             var $element = $(this.element);
+            var $clickables;
             
             if ( $element.is('form') ) {
                 
-                // function to perform if user confirms their action
                 this.yesFunction = function() {
                     // submit the form
                     $element.submit();
                 }
 
-                var $submitButtons = $(this.element).find('input[type=submit], button[type=submit], button:not([type])');
-                $submitButtons.on('click', function(event) {
-                    event.preventDefault();
-                    var $this = $(this);
-                    if ( ! $this.is('.disabled') && ! $this.prop('disabled') ) {
-                        var action = $this.attr('title');
-                        if ( typeof action === "undefined" ) {
-                            // element has no title
-                            if ( $this.is('button') ) {
-                                // element is button
-                                action = $this.text();
-                            } else {
-                                // element is an input
-                                 action = $this.val();
-                            }
-                            
-                            if ( action === "" ) {
-                                // button/input doesn't have text/a value
-                                if ( $this.parent().is('label') && $this.parent().text() !== "") {
-                                    // use the label parent's text
-                                    action = $this.parent().text();
-                                } else {
-                                    // use a general message
-                                    action = "proceed"
-                                }
-                            }
-                        }
-                        
-                        $element.actionConfirm('confirmAction', action);
-                    }
-                });
+                $clickables = $(this.element).find('input[type=submit], button[type=submit], button:not([type])');
                 
             } else if ( $element.is('button') ) {
                 
-                // function to perform if user confirms their action
                 this.yesFunction = function() {
                     // submit the closest form
                     $element.closest('form').submit();
                 }
 
-                $element.on('click', function(event) {
-                    event.preventDefault();
-                    if ( ! $element.is('.disabled') && ! $element.prop('disabled') ) {
-                        var action = coalesce($element.attr('title'), $element.text());
-                        $element.actionConfirm('confirmAction', action);
-                    }
-                });
+                $clickables = $element;
                 
             } else if ( $element.is('a') ) {
 
                 if ( ! $element.attr('href') ) {
                     // element has no href attribute
-                    throw "<a> must have an href property."
+                    $.error("<a> must have an href property.");
                 }
 
-                // function to perform if user confirms their action
                 this.yesFunction = function() {
                     // navigate to the href
                     window.location = $element.attr('href');
                 }
 
-                $element.on('click', function(event) {
-                    event.preventDefault();
-                    if ( ! $element.is('.disabled') && ! $element.prop('disabled') ) {
-                        var action = coalesce($element.attr('title'), $element.text());
-                        $element.actionConfirm('confirmAction', action);
-                    }
-                });
+                $clickables = $element;
+                
+            } else {
+                // Unsupported element
+                $.error('Unsupported element ' + $element.prop('tagName') + '. actionConfirm only supports <a>, <button>, and <form>.');
             }
+
+            // bind confirm dialog to the element's clickable item(s).
+            $clickables.on('click', function(event) {
+                event.preventDefault();
+                var $this = $(this);
+                if ( ! $this.is('.disabled') && ! $this.prop('disabled') ) {
+                    var action = $this.attr('title');
+                    if ( typeof action === "undefined" ) {
+                        // element has no title
+                        if ( $this.is('a, button') ) {
+                            // element is <button> or <a>
+                            action = $this.text();
+                        } else {
+                            // element is <input>
+                            action = $this.val();
+                        }
+                        
+                        if ( action === "" ) {
+                            // element doesn't have a value/text
+                            if ( $this.parent().is('label') && $this.parent().text() !== "") {
+                                // use the label parent's text
+                                action = $this.parent().text();
+                            } else {
+                                // use a general message
+                                action = "proceed"
+                            }
+                        }
+                    }
+                    
+                    $element.actionConfirm('confirmAction', action);
+                }
+            });
         },
 
         confirmAction: function(action) {
