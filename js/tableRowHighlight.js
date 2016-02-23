4755b0500428400f7c56bdf8dd298614f0c820bd
Priyank Jalu <priyank@qcode.co.uk>
2016-02-23 10:27:50 +0000

    Updated tableRowHighlight plugin

--- js/tableRowHighlight.js
+++ js/tableRowHighlight.js
@@ -1,7 +1,13 @@
-function tableRowHighlight(oTable) {
-    jQuery(oTable).children('tbody').on('click', 'tr', function(event) {
-	var target_td = jQuery(event.target).closest("td");
-	jQuery(this).toggleClass('highlight');
-        $(event.target).trigger('toggleHighlight');
-    });
-}
+// Table row highlighter plugin
+$.widget("qcode.tableRowHighlight", {
+    options: {
+	class: "background-yellow"
+    },
+    _create: function() {
+	this._on(this.element, {
+	    "click td": function(event) {
+		jQuery(event.target).closest("tr").toggleClass(this.options.class);
+	    }
+	});
+    }
+});
