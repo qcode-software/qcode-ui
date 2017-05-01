bf9afa04a4256f87d404f5f761493ad55cfbef90
Peter Chaplin <peter@qcode.co.uk>
2017-05-01 15:24:36 +0100

    Support filtering table contents before copying

--- js/jquery.tableCopy.js
+++ js/jquery.tableCopy.js
@@ -7,9 +7,12 @@
 */
 ;(function() {
     "use strict";
-    jQuery.fn.tableCopy = function() {
+    jQuery.fn.tableCopy = function(filter) {
         var deferred = new jQuery.Deferred();
         var clone = this.clone();
+        if ( typeof filter === "function" ) {
+            clone = filter(clone);
+        }
         var wrapper = $('<div>');
         wrapper.append(clone);
         var html = wrapper.html();
