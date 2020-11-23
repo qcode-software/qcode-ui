294097e4d8552720e169481684332917e12b38ba
Peter Chaplin <peter@qcode.co.uk>
2020-11-23 16:49:01 +0000

    jquery.dbEditorCombo tests

---
+++ test/jquery.dbEditorCombo.test.js
@@ -0,0 +1,99 @@
+beforeEach(() => {
+    require('./mocks/offsetParent.js');
+
+    const filesystem = require('fs');
+    const fragment = filesystem
+          .readFileSync('./test/jquery.dbEditorCombo.test.html');
+    document.body.innerHTML = fragment;
+    
+    jQuery.ajax = function(options) {
+        var xmlDoc = jQuery.parseXML(
+            "<records>" +
+                    "<record>" +
+                    "<option>Apple</option>" +
+                    "<option>Banana</option>" +
+                    "<option>Pear</option>" +
+                    "</record>" +
+                    "</records>"
+        );
+        options.success(xmlDoc);
+    }
+});
+
+test('jquery.dbEditorCombo.js show',() => {
+    // Mock offsetLeft for testing since jest doesn't do layout
+    Object.defineProperty($('.editor')[0], 'offsetLeft', {
+        get() {
+            return 50;
+        }
+    });
+    
+    $('.container').dbEditorCombo(
+        'show',
+        $('.editor'),
+        $('.editor').text(),
+        "/dummy.xml"
+    );
+    
+    expect( $('.container').children().length ).toBe(2);
+    expect( $('.db-editor').val() ).toBe('Fruit...');
+    expect( $('.editor')[0].offsetParent ).toBe( $('.container')[0] );
+    expect( $('.editor')[0].offsetLeft ).toBe(50);
+    expect( $('.db-editor').css('left') ).toBe("50px");
+    expect( $('.db-editor').css('borderTopWidth') ).toBe("1px");
+});
+
+test('jquery.dbEditorCombo.js getCurrentElement',() => {
+    $('.container').dbEditorCombo(
+        'show',
+        $('.editor'),
+        $('.editor').text(),
+        "/dummy.xml"
+    );
+
+    expect (
+        $('.container').dbEditorCombo('getCurrentElement')[0]
+    ).toBe( $('.editor')[0] );
+});
+
+test('jquery.dbEditorCombo.js getValue',() => {
+    $('.container').dbEditorCombo(
+        'show',
+        $('.editor'),
+        $('.editor').text(),
+        "/dummy.xml"
+    );
+
+    expect(
+        $('.container').dbEditorCombo('getValue')
+    ).toBe('Fruit...');
+});
+
+test('jquery.dbEditorCombo.js setValue',() => {
+    $('.container').dbEditorCombo(
+        'show',
+        $('.editor'),
+        $('.editor').text(),
+        "/dummy.xml"
+    );
+    $('.container').dbEditorCombo('setValue','oranges');
+
+    expect(
+        $('.container').dbEditorCombo('getValue')
+    ).toBe('oranges');
+});
+
+
+test('jquery.dbEditorCombo.js hide',() => {
+    $('.container').dbEditorCombo(
+        'show',
+        $('.editor'),
+        $('.editor').text(),
+        "/dummy.xml"
+    );
+    $('.container').dbEditorCombo('hide');
+
+    expect(
+        $('.db-editor').css('display')
+    ).toBe('none');
+});
