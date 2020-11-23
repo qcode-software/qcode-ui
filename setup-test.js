90def8bccdf0b2389e7cd2a82d76f83c5496e706
Peter Chaplin <peter@qcode.co.uk>
2020-11-23 10:17:29 +0000

    Interim commit

--- setup-test.js
+++ setup-test.js
@@ -2,7 +2,17 @@ global.window = window;
 global.$ = global.jQuery = require('./external/jquery-3.5.1.min');
 require('./external/jquery-ui-1.12.1.custom.min');
 
-eval((function(){
+(function(){
+    const fs = require('fs');
+    var source = "";
+    fs.readdirSync('./js/').sort().forEach(filename => {
+        if ( filename.slice(-3) === '.js' ) {
+            require('./js/' + filename);
+        }
+    });
+})();
+
+/*eval((function(){
     const fs = require('fs');
     var source = "";
     fs.readdirSync('./js/').sort().forEach(filename => {
@@ -11,4 +21,4 @@ eval((function(){
         }
     });
     return source;
-})());
+})());*/
