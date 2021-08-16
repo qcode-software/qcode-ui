41059231f6bdb70175d1cd8068527a383ebfefb6
Peter Chaplin <peter@qcode.co.uk>
2021-08-16 17:02:47 +0100

    Uploader test

---
+++ test/headless-browser/qcode.uploader.test.js
@@ -0,0 +1,32 @@
+describe('Qcode uploader', () => {
+    let page;
+    beforeAll(async () => {
+        page = await ready_page('qcode.uploader.test.html');
+        await page.setRequestInterception(true);
+    });
+
+    it('uploads a file', async () => {
+        const callback = jest.fn(request => request.respond({
+            status: 200
+        }));
+        page.on('request', callback);
+        const [fileChooser] = await Promise.all([
+            page.waitForFileChooser(),
+            page.click('input')
+        ]);
+        await fileChooser.accept(['test/headless-browser/samples/hello_world.txt']);
+        await page.evaluate(
+            () => {
+                const file = document.getElementsByTagName('input')[0].files[0];
+                console.log(file);
+                const uploader = new qcode.Uploader({
+                    file: file,
+                    url: "/upload"
+                });
+                uploader.start();
+            }
+        );
+        page.off('request', callback);
+        return expect(callback.mock.calls.length).toBe(1);
+    });
+});
