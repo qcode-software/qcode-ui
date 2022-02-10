7702a05cb3b27d2f2e660d5307028416a2b0daf5
Peter Chaplin <peter@qcode.co.uk>
2022-02-10 12:16:02 +0000

    qcode.colInherit plugin with passing tests

--- js/qcode.colInherit.js
+++ js/qcode.colInherit.js
@@ -3,13 +3,13 @@
 // Does not overwrite existing cell inline styles.
 // Optionally takes an array of custom attribute names to also be copied.
 ;var qcode = qcode || {};
-qcode.colInherit = function(target, options) {
+qcode.colInherit = function(target, customAttributes) {
     "use strict";
     if ( target instanceof HTMLCollection ) {
         target = Array.from(target);
     }
     if ( target instanceof Array ) {
-        for (element of target) {
+        for (const element of target) {
             qcode.colInherit(element, options);
         }
         return
@@ -22,41 +22,53 @@ qcode.colInherit = function(target, options) {
     const id = qcode.getID(target);
     const cols = qcode.tableColumns(target);
     let css = {};
-    for (let index = 0; i < cols.length; i++) {
+    for (let index = 0; index < cols.length; index++) {
         let col = cols[index];
-        for (let row of table.rows) {
-            let cell = row[index];
-            
-            for (let className of col.classList.entries()) {
+
+        if ( col.hasAttribute('style') ) {
+            const selector = `
+#${id} > tr > *:nth-child(${index+1}),
+#${id} > * >tr > *:nth-child(${index+1})`;
+            css[selector] = {};
+
+            for (const property in col.style) {
+                const value = col.style[property];
+                if ( ! (property === "display"
+                        && value === "table-column") ) {
+                    css[selector][property] = value;
+                }
+            };
+        }
+        
+        for (const row of target.rows) {
+            const cell = row.children.item(index);
+                
+            for (const className of col.classList.values()) {
                 cell.classList.add(className);
             }
 
-            if ( col.hasAttribute('style') ) {
-                var selector = `
-#${id} > tr > *:nth-child(${index+1}),
-#${id} > * >tr > *:nth-child(${index+1})`;
-                css[selector] = {};
-
-                for (property in col.style) {
-                    let value = col.style[property];
-                    if ( ! (property === "display"
-                            && value === "table-column") ) {
-                        css[selector][property] = value;
-                    }
-                });
+            for (const attributeName of (customAttributes || [])){
+                if ( col.hasAttribute(attributeName) ) {
+                    cell.setAttribute(
+                        attributeName,
+                        col.getAttribute(attributeName)
+                    );
+                }
             }
         }
     }
+    qcode.style(css);
+    return target
 }
 
 qcode.tableColumns = function(target) {
     "use strict";
     let cols = []
-    for ( child of Array.from(target.children) ) {
+    for (const child of Array.from(target.children)) {
         if ( child.tagName == 'COL' ) {
             cols.push(child);
         } else if ( child.tagName == 'COLGROUP' ) {
-            for ( grandchild of Array.from(child.children) ) {
+            for (let grandchild of Array.from(child.children)) {
                 if ( grandchild.tagName == 'COL' ) {
                     cols.push(grandchild);
                 }
