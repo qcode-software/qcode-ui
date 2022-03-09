782ecd0c5c5f2819c02c5e0e0af110c57f6cbcd1
Peter Chaplin <peter@qcode.co.uk>
2022-03-09 10:40:15 +0000

    Interim commit

--- js/qcode.textRange.js
+++ js/qcode.textRange.js
@@ -5,6 +5,7 @@
 qcode.textrange = (function(){
     "use strict";
     const get = function(element) {
+        // input element or textarea
         if ( element.matches('input, textarea') ) {
             return {
                 "selectionText": element.value.substring(
@@ -30,13 +31,14 @@ qcode.textrange = (function(){
                 "text": element.innerText
             }
         }
-        
+
+        // Use ranges 
         const selectedRange = selection.getRangeAt(0);
         
         const elementRange = document.createRange();
 	elementRange.selectNodeContents(element);
         
-        const selectionStart = rangeStartInRange(selectedRange, elementRange);
+        const selectionStart = startOfRangeInRange(selectedRange, elementRange);
         const selectionEnd = endOfRangeInRange(selectedRange, elementRange);
         
 	const editableRange = getEditableRange(element);
@@ -58,11 +60,84 @@ qcode.textrange = (function(){
     };
     
     const set = function(element, start, end) {
+        if ( element != document.activeElement ) {
+            element.focus();
+        }
+        const text = ( element.matches('input, textarea') ?
+                       element.value : element.innerText );
+
+        [start, end] = parseInputs(start, end, text);
+        
+        if ( element.matches('input, textarea') ) {
+            element.selectionStart = start;
+            element.selectionEnd = end;
+            return
+        }
+        if ( ! element.matches('[contenteditable=true]') ) {
+            return
+        }
+        const selection = window.getSelection();
+        if ( selection.rangeCount == 0 ) {
+            return
+        }
+        const range = selection.createRange();
+        const startPosition = getRangePosition(element, start);
+        const endPosition = getRangePosition(element, end);
+
+        range.setStart(startPosition.node, startPosition.offset);
+        range.setEnd(endPosition.node, endPosition.offset);
+        selection.removeAllRanges();
+        selection.addRange(range);
     };
     
     return {
         "get": get,
         "set", set
+    }
+
+    function getRangePosition(node, index) {
+        // Find the text node (possibly nested) and corresponding offset
+        // on the left of character at index from start of this node
+        const childNodes = node.childNodes;
+	const myRange =  document.createRange();
+        if ( childNodes.length ) {
+            for (let i = 0; i < childNodes.length; i++) {
+                const childNode = childNodes[i];
+		myRange.selectNode(childNode);
+                const textLength = myRange.toString().length;
+                if ( (textLength > 0 && index < textLength)
+                     || (i==childNodes.length-1 && index==textLength) ) {
+		    // The point we are looking for is in this child
+                    return getRangePosition(childNode, index);
+                }
+                index -= textLength;
+            }
+        } else {
+            return {
+                node: node,
+                offset: index
+            }
+        }
+    }
+
+    function parseInputs(start, end, text) {
+        if ( start === 'start' ) {
+            start = 0;
+        }
+        if ( start === 'end' ) {
+            start = text.length;
+        }
+        if ( end === 'start' ) {
+            end = 0;
+        }
+        if ( end === 'end' ) {
+            end = text.length;
+        }
+        if ( start === 'all' && end === undefined ) {
+            start = 0;
+            end = text.length;
+        }
+        return [start, end];
     }
 
     function getEditableRange(element) {
